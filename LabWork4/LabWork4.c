bded9210304d2ad13bb45b286a4f0187d7ed8195
Ilja Rakitin <47419715+trozl@users.noreply.github.com>
2022-09-15 16:55:39 +0300

    Добавьте файлы проекта.

---
+++ LabWork4/LabWork4.c
@@ -0,0 +1,44 @@
+﻿#include<stdio.h>
+#include<locale.h>
+#define D 2.54
+#define K 1.852
+
+void calc() {
+	int num1, num2;
+	puts("Введите число с клавиатуры");
+	scanf("%d", &num1);
+	printf("%-Введенное число %d", num1);
+	puts("\nВведите второе число с клавиатуры");
+	scanf("%d", &num2);
+	printf("%-Введенное число %d", num2);
+	printf("\nРезультат перемножения чисел равен %d", num1 * num2);
+	printf("\nРезультат деления чисел равен %d", num1 / num2);
+	printf("\nРезультат остаток от деления чисел равен %d", num1 % num2);
+	printf("\nРезультат сложения чисел равен %d", num1 + num2);
+	printf("\nРезультат вычитания чисел равен %d", num1 - num2);
+
+}
+void dmtosm() {
+	int dym;
+	float result;
+	puts("Введите целое число дюймов для перевода в сантиметры");
+	scanf("%d", &dym);
+	result = D * dym;
+	printf(" %d дюймов – это %.1f см", dym, result);
+}
+void seamtokm() {
+	int dym1;
+	float result1;
+	puts("Введите целое число Морских миль для перевода в километры");
+	scanf("%d", &dym1);
+	result1 = K * dym1 / 1000.;
+	printf(" %d морских миль – это %9.9f километров", dym1, result1);
+}
+
+
+void main() {
+	setlocale(LC_ALL, "RUS");
+	//calc();
+	//dmtosm();
+	seamtokm();
+}
